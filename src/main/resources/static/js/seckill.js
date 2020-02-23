c7fdc0076a241a83c6ec56ded86a292434d07a08
william-xu <1102793+william-xu@users.noreply.github.com>
2020-02-24 00:47:01 +0800

    计时功能修复

--- src/main/resources/static/js/seckill.js
+++ src/main/resources/static/js/seckill.js
@@ -71,75 +71,92 @@ var seckill ={
     },
     countdown: function (seckillId, nowTime, startTime, endTime) {
         var seckillBox = $('#seckill-box');
+        var startCounterSpan = $('#start_counter_span');
+        var endCounterSpan = $('#end_counter_span');
         //时间判断
         if(nowTime > endTime){
             //秒杀结束
-            seckillBox.html('秒杀结束!');
+        	endCounterSpan.html('秒杀结束!');
         }else if(nowTime < startTime){
             //秒杀未开始,计时事件绑定
-            var killTime = new Date(startTime + 1000);
-            seckillBox.countdown(killTime,function(event){
-                //时间格式
-                var format = event.strftime('秒杀倒计时: %D天 %H时 %M分 %S秒');
-                seckillBox.html(format);
+            var killTime = new Date(startTime);
+            startCounterSpan.countdown(killTime,function(event){
+                var format = event.strftime('秒杀即将开始: %D天 %H时 %M分 %S秒');
+                startCounterSpan.html(format);
                 /*时间完成后回调事件*/
-            }).on('finish.countdown',function(){
-
-                seckill.handleSeckillkill(seckillId,seckillBox);
+            }).on('finish.countdown',function(){            	
+            	seckill.startSeckill(seckillId,seckillBox,endTime,startCounterSpan);
             });
         }else{
             //秒杀开始
-            seckill.handleSeckillkill(seckillId,seckillBox);
+            seckill.startSeckill(seckillId,seckillBox,endTime,startCounterSpan);
+        }
+    },
+    startSeckill: function(seckillId, seckillBox, endTime,startCounterSpan){
+    	var endCounterSpan = $('#end_counter_span');
+    	startCounterSpan.hide();
+    	seckill.handleSeckillkill(seckillId,seckillBox);
+    	var end = new Date(endTime);
+        endCounterSpan.countdown(end,function(event){
+            var format = event.strftime('距离秒杀结束: %D天 %H时 %M分 %S秒');
+            endCounterSpan.html(format);
+            /*时间完成后回调事件*/
+        }).on('finish.countdown',function(){
+        	seckillBox.hide();
+        	endCounterSpan.html("秒杀已经结束！");        	
+        });
+    },    
+    bindPhone: function(){
+    	//手机验证和登录 , 计时交互
+        //规划我们的交互流程
+        //在cookie中查找手机号
+        var killPhone = $.cookie('killPhone');
+        //验证手机号
+        if(!seckill.validatePhone(killPhone)){
+            //绑定phone
+            //控制输出
+            var killPhoneModal = $('#killPhoneModal');
+            //显示弹出层
+            killPhoneModal.modal({
+                show: true,//显示弹出层
+                backdrop: 'static',//禁止位置关闭
+                keyboard: false//关闭键盘事件
+            });
+            $('#killPhoneBtn').click(function(){
+                var inputPhone = $('#killPhoneKey').val();
+                console.log('inputPhone='+inputPhone);//TODO
+                if(seckill.validatePhone(inputPhone)){
+                    //电话写入cookie
+                    $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
+                    //刷新页面
+                    window.location.reload();
+                }else{
+                    $('#killPhoneMessage').hide().html('<label class="label label-danger">手机号错误!</label>').show(300);
+                }
+            });
         }
     },
     //详情页秒杀逻辑
     detail: {
         //详情页初始化
         init : function(params){
-            //手机验证和登录 , 计时交互
-            //规划我们的交互流程
-            //在cookie中查找手机号
-            var killPhone = $.cookie('killPhone');
-            //验证手机号
-            if(!seckill.validatePhone(killPhone)){
-                //绑定phone
-                //控制输出
-                var killPhoneModal = $('#killPhoneModal');
-                //显示弹出层
-                killPhoneModal.modal({
-                    show: true,//显示弹出层
-                    backdrop: 'static',//禁止位置关闭
-                    keyboard: false//关闭键盘事件
-                });
-                $('#killPhoneBtn').click(function(){
-                    var inputPhone = $('#killPhoneKey').val();
-                    console.log('inputPhone='+inputPhone);//TODO
-                    if(seckill.validatePhone(inputPhone)){
-                        //电话写入cookie
-                        $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
-                        //刷新页面
-                        window.location.reload();
-                    }else{
-                        $('#killPhoneMessage').hide().html('<label class="label label-danger">手机号错误!</label>').show(300);
-                    }
-                });
-            }
-            //已经登录
-            //计时交互
             var startTime = params['startTime'];
             var endTime = params['endTime'];
             var seckillId = params['seckillId'];
             $.get(seckill.URL.now(), {}, function (result) {
                 if(result && result['success']){
                     var nowTime = result['data'];
-                    //时间判断,计时交互
-                    seckill.countdown(seckillId,nowTime,startTime,endTime);
+                    if(nowTime < endTime){
+                    	seckill.bindPhone();
+                        //时间判断,计时交互
+                        seckill.countdown(seckillId,nowTime,startTime,endTime);
+                    }else{
+                    	$('#end_counter_span').html("秒杀已经结束！");
+                    }
                 }else{
                     console.log('result:'+result);
                 }
             });
-
-
         }
     }
 }
